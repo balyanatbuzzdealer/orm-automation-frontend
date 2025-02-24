6dcb173c6b44eb3f2bf2aaf888b3c608cdcbb5ca
davidatbuzzdealer <davidb@buzzdealer.com>
2025-02-24 12:33:03 +0200

    removed zustand

--- app/login/Login.tsx
+++ app/login/Login.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import { useAppStore } from "../store";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -10,50 +9,16 @@ export default function Login() {
 
   const router = useRouter();
 
-  const makeTrue = useAppStore((state) => state.makeTrue);
-  const loggedIn = useAppStore((state) => state.loggedIn);
-
   const prod_url =
     "https://orm-automation-tool-0494f308f710.herokuapp.com/login";
   const dev_url = "http://127.0.0.1:8000/login";
 
   const BACKEND_URL = dev_url;
 
-  async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    const formData = new FormData();
-
-    formData.append("email", String(email));
-    formData.append("password", String(password));
-
-    try {
-      const response = await fetch(BACKEND_URL, {
-        method: "POST",
-        body: formData,
-      });
-
-      const data = await response.json();
-
-      if (data.login === "authenticated") {
-        makeTrue();
-        router.push("/");
-      } else {
-        alert("Invalid email or password");
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
   return (
     <div className="login-form-wrapper">
       <h2>Login</h2>
-      <form
-        onSubmit={(e) => {
-          handleLogin(e);
-        }}
-      >
+      <form onSubmit={(e) => {}}>
         <div className="form-group">
           <label htmlFor="email">Email: </label>
           <input
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,9 @@
 import { useEffect } from "react";
 import ScraperInputForm from "./components/ScraperInputForm";
 import { useRouter } from "next/navigation";
-import { useAppStore } from "./store";
 
 export default function Home() {
-  const loggedIn = useAppStore((state) => state.loggedIn);
   const router = useRouter();
-
-  useEffect(() => {
-    // if (!loggedIn) {
-    //   router.push("/login");
-    // }
-  }, [loggedIn, router]);
 
   return (
     <>
